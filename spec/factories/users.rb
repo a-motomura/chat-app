d03d3cc1f313247868efc5577211cd68fae2685c
a-motomura <blackuriboh@gmail.com>
2020-10-28 16:59:06 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 8)
+    password = {Faker::Internet.password(min_length: 8)}
     password {password}
     password_confirmation {password}
   end
